4edefafdf707b4b8d4a1f54a6986ad4e8538c751
西安-谢鹏飞 <xpf14561456@163.com>
2023-06-08 22:21:25 +0800

    flyWeight模式

--- src/main/java/com/design/flyweight/BigString.java
+++ src/main/java/com/design/flyweight/BigString.java
@@ -7,13 +7,26 @@ package com.design.flyweight;
 public class BigString {
     private BigChar[] bigChars;
 
-    public BigString(String string) {
+    public BigString(String string, boolean shared) {
         bigChars = new BigChar[string.length()];
+        if (shared) {
+            initShared(string);
+        } else {
+            initUnShared(string);
+        }
+    }
+
+    private void initShared(String string) {
         BigCharFactory factory = BigCharFactory.getInstance();
         for (int i = 0; i < string.length(); i++) {
             bigChars[i] = factory.getBigChar(string.charAt(i));
         }
-        factory.printSize();
+    }
+
+    private void initUnShared(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            bigChars[i] = new BigChar(string.charAt(i));
+        }
     }
 
     public void print() {
--- src/main/java/com/design/flyweight/Main.java
+++ src/main/java/com/design/flyweight/Main.java
@@ -5,13 +5,34 @@ package com.design.flyweight;
  * @since 2023/6/8
  */
 public class Main {
+
+    private static BigString[] bigStrings = new BigString[1000];
+
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Usage: java Main digits");
-            System.out.println("Example: java Main 1212123");
-            System.exit(0);
-        }
-        BigString bigString = new BigString(args[0]);
-        bigString.print();
+//        if (args.length == 0) {
+//            System.out.println("Usage: java Main digits");
+//            System.out.println("Example: java Main 1212123");
+//            System.exit(0);
+//        }
+//        BigString bigString = new BigString(args[0], true);
+//        bigString.print();
+        System.out.println("共享时：");
+        testAllocation(true);
+        System.out.println("不共享时：");
+        testAllocation(false);
     }
+
+    public static void testAllocation(boolean shared) {
+        for (int i = 0; i < bigStrings.length; i++) {
+            bigStrings[i] = new BigString("1212123", shared);
+        }
+        showMemory();
+    }
+
+    public static void showMemory() {
+        Runtime.getRuntime().gc();
+        long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println("使用内存：" + used);
+    }
+
 }
